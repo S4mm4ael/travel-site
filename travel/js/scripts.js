0e2919b90010f2cde984a5c16908131c7521bbe5
S4mm4el <homer1996@gmail.com>
2022-06-27 14:59:45 +0300

    feat: add header

--- travel/js/scripts.js
+++ travel/js/scripts.js
@@ -1,36 +1,41 @@
 // Custom Scripts
-// Burger menu
+// Мобильное меню бургер
 function burgerMenu() {
   const burger = document.querySelector('.burger')
   const menu = document.querySelector('.menu')
   const body = document.querySelector('body')
+  const navBtns = document.querySelector('.nav__btns')
   burger.addEventListener('click', () => {
     if (!menu.classList.contains('active')) {
       menu.classList.add('active')
       burger.classList.add('active-burger')
       body.classList.add('locked')
+      navBtns.classList.add('active')
     } else {
       menu.classList.remove('active')
       burger.classList.remove('active-burger')
       body.classList.remove('locked')
+      navBtns.classList.remove('active')
     }
   })
-  // Navbar breakpoint
+  // Вот тут мы ставим брейкпоинт навбара
   window.addEventListener('resize', () => {
     if (window.innerWidth > 991.98) {
       menu.classList.remove('active')
       burger.classList.remove('active-burger')
       body.classList.remove('locked')
+      navBtns.classList.remove('active')
     }
   })
 }
 burgerMenu()
 
 
-// Fixed menu when scrolling
+// Вызываем эту функцию, если нам нужно зафиксировать меню при скролле.
 function fixedNav() {
   const nav = document.querySelector('nav')
 
+  // тут указываем в пикселях, сколько нужно проскроллить что бы наше меню стало фиксированным
   const breakpoint = 1
   if (window.scrollY >= breakpoint) {
     nav.classList.add('fixed__nav')
@@ -40,4 +45,3 @@ function fixedNav() {
 }
 window.addEventListener('scroll', fixedNav)
 
-;
